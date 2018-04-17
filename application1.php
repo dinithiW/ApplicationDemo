4d30c0705109e161128f8c8bc1b59b6558dfa002
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-04-17 18:01:27 +0530

    add first row of table

---
+++ application1.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="applicationStyle.css" type="text/css">
+
+<div id = "headDiv">
+    <div id = "headText">
+        STAFF RECRUITMENT
+    </div>
+
+</div>
+
+<div id = "contentDiv">
+    <table>
+        <tr>
+            <td>Post applied for:</td>
+            <td colspan="20"><input type="checkbox" name = "position1" value="probLec">Lecturer(Probationary)</td>
+            <td><input type="checkbox" name="position2" value="senLec">Senior Lecturer Gr. II</td>
+        </tr>
+
+    </table>
+</div>
+
+
+<div id = "footerDiv">
+    This is footer
+</div>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dinithi W
+ * Date: 4/15/2018
+ * Time: 5:47 PM
+ */
+?>
